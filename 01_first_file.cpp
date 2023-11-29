3cebceb52991e634fe26c039b88f880b85b894aa
27481 <utkarshpandey519@gmail.com>
2023-11-29 22:22:41 +0530

    first commit

---
+++ 01_first_file.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+typedef unsigned long long int ll;
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef Online_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    cout<<"testing"<<"\n";      
+ 
+    return 0;
+}
